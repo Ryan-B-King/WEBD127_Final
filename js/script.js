96a8744b921faf5d59a8c814cdd9d7ffff13899d
Ryan King <33941531+Ryan-B-King@users.noreply.github.com>
2021-05-23 19:26:16 -0700

    update
    added content to site

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
+const accordionItemHeaders = document.querySelectorAll ('.accordion-item-header')
+
+
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
 
+accordionItemHeaders.forEach(accordionItemHeader => {
+    accordionItemHeader.addEventListener('click', event => {
+        const currentlyActiveAccordionItemHeader = document.querySelector('.accordion-item-header.active')
+        if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+            currentlyActiveAccordionItemHeader.classList.toggle('active')
+            currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight=0
+        }
+
+        accordionItemHeader.classList.toggle('active')
+        const accordionItemBody = accordionItemHeader.nextElementSibling
+        if(accordionItemHeader.classList.contains('active')) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + 'px'
+        } else {
+            accordionItemBody.style.maxHeight = 0
+        }
+    })
+})
